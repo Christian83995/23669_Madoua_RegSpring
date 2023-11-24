d35eea99e650207e1173f0284e046f4c8930e248
Christian Madaoua <cmadaoua@gmail.com>
2023-11-24 13:26:38 +0100

    23669_RegWebTech

---
+++ 23669_Madaoua/23669_Madaoua/src/main/java/com/example/webtech_spring_mvc/ApplicationRegistration.java
@@ -0,0 +1,13 @@
+package com.example.webtech_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRegistration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationRegistration.class, args);
+	}
+
+}
